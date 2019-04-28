e38539b4fd447562208ad2f17fd8eef6aa5d851a
jamesmeyers88 <jamesmeyers88@gmail.com>
2019-04-28 11:01:12 -0500

    made prod build and exported the staticfile

--- out/_next/static/runtime/webpack.js
+++ out/_next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6f3cdbd53f46020f78ce";
+/******/ 	var hotCurrentHash = "9087c36e6257bafc7fa1";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
